4e10eb4afd55d55a67b085a61d9effc3fc9d5191
Logesh <jvlcodevideos@gmail.com>
2021-08-17 20:45:40 +0530

    Intial code

---
+++ application/views/ajax.php
@@ -0,0 +1,14 @@
+<div id="demo">
+  <h2>Let AJAX change this text</h2>
+  <button type="button" >Change Content</button>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+
+	$("button").click(function(){
+		$.ajax({url: "settings", success: function(result){
+			$("#demo").html(result);
+		}});
+	});
+
+</script>
